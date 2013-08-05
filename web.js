4ee4999bf2481fbfa2c60ab7bf133a4e46d572c1
Ubuntu <ubuntu@ip-172-31-42-199.us-west-2.compute.internal>
2013-08-05 18:10:31 +0000

    Edited product desc in index.html

--- web.js
+++ web.js
@@ -5,11 +5,13 @@ var myfile = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
+  app.use(express.static(__dirname + '/img'));
+  app.use(express.static(__dirname + '/css'));
   buffy = myfile.readFileSync('index.html');
   response.send(buffy.toString());
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
